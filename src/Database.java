0876e70b25e39e491b445148118f6b5839d47e8a
zh-crystal <zh980801@outlook.com>
2020-06-14 21:28:54 +0800

    zyl

--- src/Database.java
+++
@@ -1,6 +0,0 @@
-public class Database {
-	String driverName="com.microsoft.sqlserver.jdbc.SQLServerDriver";//SQL
-	String url="jdbc:sqlserver://localhost:1433;DatabaseName=Information";
-	String user="sa";
-	String password="123456";
-}
